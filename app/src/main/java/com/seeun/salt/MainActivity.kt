a575f1e6702091b861d7cb49eb5c2c7e4c4e3040
eunpse <69415018+eunpse@users.noreply.github.com>
2022-10-03 22:38:49 +0900

    Calendar Custom

--- app/src/main/java/com/seeun/salt/MainActivity.kt
+++ app/src/main/java/com/seeun/salt/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.seeun.salt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import com.seeun.salt.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val monthListManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val monthListAdapter = AdapterMonth()
+
+        binding.calendarCustom.apply {
+            layoutManager = monthListManager
+            adapter = monthListAdapter
+        }
+        //val snap = PagerSnapHelper()
+        //snap.attachToRecyclerView(calendar_custom)
     }
 }
