7cb8485bcd5368c418b36124675e931e1b755c7e
eunpse <69415018+eunpse@users.noreply.github.com>
2022-11-06 23:05:26 +0900

    calender, login, register all successfull!!!!! YaY

--- app/src/main/java/com/seeun/salt/MainActivity.kt
+++ app/src/main/java/com/seeun/salt/MainActivity.kt
@@ -1,28 +1,25 @@
 package com.seeun.salt
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
-import com.seeun.salt.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity :AppCompatActivity(){
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+    //달력
         val monthListManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        val monthListAdapter = AdapterMonth()
+        val monthListAdapter = AdapterMonthActivity()
 
-        binding.calendarCustom.apply {
+        calendar_custom.apply {
             layoutManager = monthListManager
             adapter = monthListAdapter
             scrollToPosition(Int.MAX_VALUE/2)
         }
+
         val snap = PagerSnapHelper()
         snap.attachToRecyclerView(calendar_custom)
     }
