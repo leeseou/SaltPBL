27241b81c2499612fc56426d75c9338309812b71
dangeunii <impine0.0r@gmail.com>
2022-11-11 15:51:54 +0900

    feat: splash 화면 생성
    splash 화면 생성 및 기능 구현

--- app/src/main/java/com/seeun/salt/LoginActivity.kt
+++ app/src/main/java/com/seeun/salt/LoginActivity.kt
@@ -1,11 +1,11 @@
 package com.seeun.salt
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class LoginnActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loginn)
+        setContentView(R.layout.activity_login)
     }
 }
